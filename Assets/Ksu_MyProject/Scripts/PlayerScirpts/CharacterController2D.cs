33f00f547175e38aa8ea0656ec0fae534aa5f25e
whdrkwlq14 <131209733+whdrkwlq14@users.noreply.github.com>
2023-10-16 15:25:47 +0900

    10.16
    ++

--- Assets/Ksu_MyProject/Scripts/PlayerScirpts/CharacterController2D.cs
+++ Assets/Ksu_MyProject/Scripts/PlayerScirpts/CharacterController2D.cs
@@ -186,7 +186,6 @@ public class CharacterController2D : MonoBehaviour
         animator.SetBool("IsDead", true);
         canMove = false;
         invincible = true;
-        GetComponent<Attack>().enabled = false;
         yield return new WaitForSeconds(0.4f);
         m_Rigidbody2D.velocity = new Vector2(0, m_Rigidbody2D.velocity.y);
         yield return new WaitForSeconds(1.1f);
