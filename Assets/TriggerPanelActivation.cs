773f678434e2b63fcf8a6066c48afe04e4289da3
whdrkwlq14 <131209733+whdrkwlq14@users.noreply.github.com>
2023-10-16 09:11:18 +0900

    10.16
    토글키

--- Assets/TriggerPanelActivation.cs
+++ Assets/TriggerPanelActivation.cs
@@ -2,13 +2,36 @@ using UnityEngine;
 
 public class TriggerPanelActivation : MonoBehaviour
 {
+    public GameObject E_Panel; //E_Panel 적용할 패널
+    public GameObject TextPanel; // E키를 눌렀을때 불러올 textPanel 패널
 
-    public void OnTriggerStay(Collider other)
+    private bool canInterct = false; // 상호작용 초기화
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // 플레이어와 충돌했을 때 실행할 동작
-            Debug.Log("플레이어와 충돌했습니다.");
+            canInterct = true;
+            E_Panel.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInterct=false;
+            TextPanel.SetActive(false);
+            E_Panel.gameObject.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        if (canInterct && Input.GetKeyDown(KeyCode.E))
+        {
+            TextPanel.SetActive(!TextPanel.activeSelf);
+            E_Panel.SetActive(!E_Panel.activeSelf);
         }
     }
 }
